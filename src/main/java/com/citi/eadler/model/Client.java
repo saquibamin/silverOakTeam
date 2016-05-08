0164862d5b8701ae3e504407b4bbb9dbabb1ef3c
saquibamin <sqb.amin@gmail.com>
2016-05-08 04:10:54 -0700

    adding source code

---
+++ src/main/java/com/citi/eadler/model/Client.java
@@ -0,0 +1,11 @@
+package com.citi.eadler.model;
+
+
+public class Client {
+
+	public static void verify(){
+		
+		System.out.println("this is maven project");
+		
+	}
+}
---
+++ src/main/java/com/citi/eadler/model/EdealerMainClass.java
@@ -0,0 +1,12 @@
+package com.citi.eadler.model;
+
+
+public class EdealerMainClass {
+
+	public static void main(String[] args) {
+		Client.verify();
+		System.out.println("setup completed ....");
+
+	}
+
+}
